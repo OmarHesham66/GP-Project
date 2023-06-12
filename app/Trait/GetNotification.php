4b9a85a5540c4a119f2191eb1971af46ef99b40b
OmarHesham <omarhesham11a@gmail.com>
2023-06-12 19:54:28 +0300

    edit-notification

--- app/Trait/GetNotification.php
+++ app/Trait/GetNotification.php
@@ -9,6 +9,6 @@ trait GetNotification
     use CheckSession;
     public function get_notification()
     {
-        return Notification::with('follow', 'sender', 'comment', 'replay', 'user', 'post')->where('user_id', '=', $this->get_session()->id)->get();
+        return Notification::with('follow', 'sender', 'comment', 'replay', 'user', 'post')->where('user_id', '=', $this->get_session()->id)->where('user_sender', '!=', $this->get_session()->id)->get();
     }
 }
